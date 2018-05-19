42d24b9d27c69cf78029a7ede229fe4ed38173a4
agritag <a.garnizone@gmail.com>
2018-05-19 13:27:54 +0300

    grp_in_R: kernlab results

--- grp_in_R/R/kernlab_train_GP_model_SARCOS.R
+++ grp_in_R/R/kernlab_train_GP_model_SARCOS.R
@@ -3,9 +3,20 @@ library(dplyr)
 library(reshape2)
 library(kernlab)
 
-# read in airplane data set ----------------------------------------------------
-df <- read.csv("../data_sources/Airline_data_set/airline_standardized.csv",
-               stringsAsFactors = FALSE)
+# read in SARCOS data set ----------------__------------------------------------
+# data set for training
+df_sarcos <- 
+ rmatio::read.mat("http://www.gaussianprocess.org/gpml/data/sarcos_inv.mat") %>%
+ as.data.frame()
+df_sarcos <- df_sarcos[,1:22]
+names(df_sarcos) <- c(paste("feature", 1:21, sep = "_"), "target")
+
+# data set for testing
+df_sarcos_test <- 
+ rmatio::read.mat("http://www.gaussianprocess.org/gpml/data/sarcos_inv_test.mat") %>%
+ as.data.frame()
+df_sarcos_test <- df_sarcos_test[,1:22]
+names(df_sarcos_test) <- c(paste("feature", 1:21, sep = "_"), "target")
 
 # MAE and RMSE  ---------------------------------------------------------------- 
 source("R/calculate_MAE.R")
@@ -13,12 +24,9 @@ source("R/calculate_RMSE.R")
 
 # Main part: test various kernels ----=-----------------------------------------
 
-# create sample set size sequence from original data frame == df_train, starting
-# with n=100 and increasing size by 10^4 or 10^6 until reaches nrow of data 
-# frame
-sample_size_first_part = seq(10^2, 10^6, by = 10^(3))
-sample_size_second_part = seq(10^6, nrow(df_train), by = 10^6)
-sample_size = c(sample_size_first_part, sample_size_second_part, nrow(df_train))
+# create sample set size sequence from original data frame == df_sarcos, 
+#starting with n=100 and increasing size by 500 until reaches nrow of data frame
+sample_size = c(seq(100, nrow(df_sarcos), by = 500), df_sarcos)
 
 # initialize empty data frame to record metrics
 experiment_data <- data.frame(sample_size = NA, 
@@ -28,22 +36,21 @@ experiment_data <- data.frame(sample_size = NA,
                               time.taken.test = NA)
 
 # create vector of kernels:
-methods <- c("rbfdot", "laplacedot", "vanilladot")
+kernels <- c("rbfdot", "laplacedot", "vanilladot")
 
 
 # go through all sample sizes while it runs out of memory
 # loop will take some time while going through all sample sets
 for (kernel in kernels) {
   for (i in sample_size){
-    sample_rows <- sample(nrow(df_train), size = i, replace = FALSE)
-    x_sample <- df_train[sample_rows,(!(names(df_train) %in% "ArrDelay"))]
-    y_sample <- df_train[sample_rows,"ArrDelay"]
+    sample_rows <- sample(nrow(df_sarcos), size = i, replace = FALSE)
+    x_sample <- df_sarcos[sample_rows,(!(names(df_sarcos) %in% "target"))]
+    y_sample <- df_sarcos[sample_rows,"target"]
     
     start.time <- Sys.time()
     tryCatch(
       gausspr_model <- gausspr(x_sample, y_sample, variance.model=T,
-                               kerne='rbfdot',
-                               kpar=list(sigma = 0.005)),
+                               kerne=kernel),
       error = function(error){
         print((paste("Error while creating model for sample with size of",
                      as.character(i))))
@@ -54,14 +61,15 @@ for (kernel in kernels) {
     time.taken <- difftime(end.time, start.time, units = "secs")
     
     start.time <- Sys.time()
-    predicted_values = predict(gausspr_model, df_test[,(!(names(df_test) %in% 
-                                                            "ArrDelay"))]) %>%
-      as.vector()
+    predicted_values = predict(gausspr_model, 
+                               df_sarcos_test[,(!(names(df_sarcos_test) %in% 
+                                                            "target"))]) %>%
+                       as.vector()
     end.time <- Sys.time()
     time.taken.test <- difftime(end.time, start.time, units = "secs")
     
-    RMSE_value = calculate_RMSE(df_test[,"ArrDelay"], predicted_values)
-    MAE_value = calculate_MAE(df_test[,"ArrDelay"], predicted_values)
+    RMSE_value = calculate_RMSE(df_sarcos_test[,"target"], predicted_values)
+    MAE_value = calculate_MAE(df_sarcos_test[,"target"], predicted_values)
     
     experiment_data <- rbind(experiment_data,
                              data.frame(sample_size = i,
@@ -74,7 +82,7 @@ for (kernel in kernels) {
                 "in time", as.character(time.taken)))
   }
   
-  file_name <- paste0("kernlab_airline_", kernel,"_sample.csv")
+  file_name <- paste0("kernlab_SARCOS_", kernel,"_sample.csv")
   write.csv(experiment_data, kernel,
             row.names = FALSE)
 }
